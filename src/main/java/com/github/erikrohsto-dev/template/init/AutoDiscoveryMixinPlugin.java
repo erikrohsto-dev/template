8a3ef0735fd2fd5e7bb159c0a650c07a09ad4218
GitHub Action <action@github.com>
2026-06-07 14:52:43 +0000

    Initialize template repository proper
    Set package name to com.github.erikrohsto-dev.template
    Set project name to template
    Set modid to template

--- src/main/java/com/github/erikrohsto-dev/template/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/erikrohsto-dev/template/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.erikrohsto-dev.template.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
--- src/main/java/com/github/erikrohsto-dev/template/mixin/MixinGuiMainMenu.java
+++ src/main/java/com/github/erikrohsto-dev/template/mixin/MixinGuiMainMenu.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package com.github.erikrohsto-dev.template.mixin;
 
 import net.minecraft.client.gui.GuiMainMenu;
 import org.spongepowered.asm.mixin.Mixin;
